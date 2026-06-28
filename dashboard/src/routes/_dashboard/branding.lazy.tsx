84f59fb1945f7e9a3ed60dc6ed585a4c7b68562e
saniyar-dev <saniyar.dev@gmail.com>
2026-06-28 04:18:05 +0330

    fix(dashboard): visuals of branding

--- dashboard/src/routes/_dashboard/branding.lazy.tsx
+++ dashboard/src/routes/_dashboard/branding.lazy.tsx
@@ -1,17 +1,22 @@
-import { Page } from '@marzneshin/common/components'
-import { BrandingWidget } from '@marzneshin/modules/settings'
-import { createLazyFileRoute } from '@tanstack/react-router'
-import { useTranslation } from 'react-i18next'
+import { Page, VStack } from "@marzneshin/common/components";
+import { BrandingWidget } from "@marzneshin/modules/settings";
+import { createLazyFileRoute } from "@tanstack/react-router";
+import { useTranslation } from "react-i18next";
 
 export const Branding = () => {
-    const { t } = useTranslation()
+    const { t } = useTranslation();
     return (
-        <Page title={t('branding')}>
-            <BrandingWidget />
+        <Page
+            title={t("branding")}
+            className="sm:flex flex-col lg:grid grid-cols-2 gap-3 h-full"
+        >
+            <VStack className="gap-3">
+                <BrandingWidget />
+            </VStack>
         </Page>
-    )
-}
+    );
+};
 
-export const Route = createLazyFileRoute('/_dashboard/branding')({
+export const Route = createLazyFileRoute("/_dashboard/branding")({
     component: Branding,
-})
+});
